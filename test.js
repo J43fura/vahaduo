ee13af1dcff6a0991a9847769ebbc71758d0fc05
J43fura <mehdi.jaafourag@gmail.com>
2022-11-11 22:52:24 +0100

    early commit

---
+++ test.js
@@ -0,0 +1,42 @@
+// Check if Marker's name correlates to a country adjectival
+function Correlation(Adjectival, Marker) {
+  if (Marker.includes(Adjectival)) {
+    console.log("INCLUDED");
+  } else {
+    console.log("NOT INCLUDED");
+  }
+}
+
+Correlation(
+
+	for (i=1; i<document.getElementById("distances").rows.length; i++){
+		//name
+		name = document.getElementById("distances").rows[i].cells[1].innerHTML;
+		//color
+		color = document.getElementById("distances").rows[i].cells[0].getAttribute('style').split(';')[1];
+		//
+		f[]
+		f['name'] = 'color'
+
+			}
+  "Tunisian",
+  "Tunisian_Guebelli_106064.626421.6245798.010265.016545",
+  //color
+  document.getElementById("distances").rows[1].cells[0].getAttribute('style').split(';')[1],
+  >'background-color:hsl(39.41378963529803, 100%, 50%)',
+  //Marker name
+  document.getElementById("distances").rows[1].cells[1].innerHTML,
+  //rows 1 -> n cells 0 - 1
+
+
+  //ychouf kel esm lbled mawjoud
+  //ken ey yeou el loun 
+  // yedhen el bled bel loun1332-1248
+
+
+
+);
+
+//how to execute js code in terminal visual studio code
+//miss you
+//vahuduo 25 markers
