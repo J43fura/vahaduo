505f7128f173cfd806d70d3f7bc96aee04783049
J43fura <mehdi.jaafourag@gmail.com>
2022-11-12 01:54:54 +0100

    TODO: torbt w tlawwn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const CaC = JSON.parse(localStorage.getItem("myObject")); //Countries and colors
 lat = 37;
 lng = 10;
 var map = L.map("map").setView([lat, lng], 5);
@@ -61,18 +62,5 @@ map.on("click", function (e) {
     }
   }
 });
-
-function hslToHex(h, s, l) {
-  l /= 100;
-  const a = (s * Math.min(l, 1 - l)) / 100;
-  const f = (n) => {
-    const k = (n + h / 30) % 12;
-    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
-    return Math.round(255 * color)
-      .toString(16)
-      .padStart(2, "0"); // convert to Hex and prefix "0" if needed
-  };
-  return `#${f(0)}${f(8)}${f(4)}`;
-}
 
 // Tiles: OpenTopoMap
